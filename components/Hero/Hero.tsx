3c4b041ccb68105c3453387064f4d13ac8a0555a
UPSAtwal <UPSAtwal@users.noreply.github.com>
2023-02-21 16:51:06 +0530

    comment out h1

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <main className={`${styles.container}`}>
             <div>
-                <h1 className={`${styles.main}`}>E-SUMMIT &apos;23</h1>
+                {/* <h1 className={`${styles.main}`}>E-SUMMIT &apos;23</h1> */}
                 <p className={styles.content}>
                     E-Summit &apos;23 aims to provide a platform for discussion on innovative ideas that have the potential to transform current challenges into opportunities for positive change. The goal of the event is to inspire and offer rays of hope to attendees.
                 </p>
